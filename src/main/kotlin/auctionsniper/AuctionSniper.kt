8531eab79faad0ab619fe56b96853afe49a44777
welldoer <welldoer@263.net>
2020-12-12 23:58:49 +0800

    [RED] Extracted auction sniper behaviour into its own class

---
+++ src/main/kotlin/auctionsniper/AuctionSniper.kt
@@ -0,0 +1,11 @@
+package auctionsniper
+
+class AuctionSniper(private val sniperListener: SniperListener) : AuctionEventListener {
+    override fun auctionClosed() {
+        sniperListener.sniperLost()
+    }
+
+    override fun currentPrice(price: Int, increment: Int) {
+        TODO("Not yet implemented")
+    }
+}
