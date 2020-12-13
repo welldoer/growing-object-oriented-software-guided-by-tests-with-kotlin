9035f685ea158e9c8bd4e41b464f8c824725a008
welldoer <welldoer@263.net>
2020-12-13 17:58:11 +0800

    [GREEN] Extract auction event parsing into a separate class

--- src/main/kotlin/auctionsniper/AuctionMessageTranslator.kt
+++ src/main/kotlin/auctionsniper/AuctionMessageTranslator.kt
@@ -8,13 +8,12 @@ class AuctionMessageTranslator(listener: AuctionEventListener) : MessageListener
     private val listener = listener
 
     override fun processMessage(chat: Chat?, message: Message) {
-        val event = unpackEventFrom(message)
-        val type = event["Event"]
+        val event = AuctionEvent.from(message.body)
+        val type = event.type()
         if ("CLOSE" == type) {
             listener.auctionClosed()
         } else if ("PRICE" == type) {
-            listener.currentPrice(Integer.parseInt(event["CurrentPrice"]),
-                                    Integer.parseInt(event["Increment"]))
+            listener.currentPrice(event.currentPrice(), event.increment())
         }
     }
 
@@ -26,5 +25,49 @@ class AuctionMessageTranslator(listener: AuctionEventListener) : MessageListener
         }
 
         return event
+    }
+
+    private class AuctionEvent {
+        companion object {
+            fun from(messageBody: String): AuctionEvent {
+                val event = AuctionEvent()
+                for (field in fieldsIn(messageBody)) {
+                    event.addField(field)
+                }
+
+                return event
+            }
+
+            fun fieldsIn(messageBody: String): List<String> {
+                return messageBody.split(";").dropLastWhile { it.isEmpty() }
+            }
+        }
+
+        private val fields = HashMap<String, String>()
+
+        fun type(): String {
+            return get("Event")
+        }
+
+        fun currentPrice(): Int {
+            return getInt("CurrentPrice")
+        }
+
+        fun increment(): Int {
+            return getInt("Increment")
+        }
+
+        private fun getInt(fieldName: String): Int {
+            return get(fieldName).toInt()
+        }
+
+        private operator fun get(fieldName: String): String {
+            return fields[fieldName]!!
+        }
+
+        private fun addField(field: String) {
+            val pair = field.split(":").dropLastWhile { it.isEmpty() }.map { it.trim() }
+            fields[pair[0]] = pair[1]
+        }
     }
 }
