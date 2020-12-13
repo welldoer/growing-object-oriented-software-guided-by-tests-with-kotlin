4c124a4126cb34c06c7515758cffc2458d56b79d
welldoer <welldoer@263.net>
2020-12-13 19:08:13 +0800

    [GREEN] Made sniper win the auction when it is holding the winning bid

--- src/test/kotlin/unit/auctionsniper/AuctionSniperTest.kt
+++ src/test/kotlin/unit/auctionsniper/AuctionSniperTest.kt
@@ -13,12 +13,43 @@ class AuctionSniperTest {
     private val sniperListener = context.mock(SniperListener::class.java)
     private val auction = context.mock(Auction::class.java)
     private val sniper = AuctionSniper(auction, sniperListener)
+    private val sniperState = context.states("sniper")
 
     @Test
-    fun `reports lost when auction closes`() {
+    fun `reports lost when auction closes immediately`() {
         context.expect {
             oneOf(sniperListener).sniperLost()
         }.whenRunning {
+            sniper.auctionClosed()
+        }
+    }
+
+    @Test
+    fun `report lost if auction closes when bidding`() {
+        context.expect {
+            ignoring(auction)
+            allowing(sniperListener).sniperBidding()
+            then(sniperState.`is`("bidding"))
+
+            atLeast(1).of(sniperListener).sniperLost()
+            `when`(sniperState.`is`("bidding"))
+        }.whenRunning {
+            sniper.currentPrice(123, 45, PriceSource.FromOtherBidder)
+            sniper.auctionClosed()
+        }
+    }
+
+    @Test
+    fun `report won if auction closes when winning`() {
+        context.expect {
+            ignoring(auction)
+            allowing(sniperListener).sniperWinning()
+            then(sniperState.`is`("winning"))
+
+            atLeast(1).of(sniperListener).sniperWon()
+            `when`(sniperState.`is`("winning"))
+        }.whenRunning {
+            sniper.currentPrice(123, 45, PriceSource.FromSniper)
             sniper.auctionClosed()
         }
     }
